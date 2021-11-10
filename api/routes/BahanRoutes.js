795213411764aea69114ce7296341996645c29c1
jonathan-cj <13519144@std.stei.itb.ac.id>
2021-11-10 14:30:12 +0700

    add authenticator to routes

--- api/routes/BahanRoutes.js
+++ api/routes/BahanRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const bahanController = require("../controllers/BahanController");
+const Authenticator = require("../middleware/Authenticator");
 
-router.get("/", bahanController.index);
-router.get("/:id", bahanController.detail);
-router.post("/create", bahanController.create);
-router.put("/update", bahanController.update);
-router.get("*", bahanController.notFound);
+router.get("/", Authenticator, bahanController.index);
+router.get("/:id", Authenticator, bahanController.detail);
+router.post("/create", Authenticator, bahanController.create);
+router.put("/update", Authenticator, bahanController.update);
+router.get("*", Authenticator, bahanController.notFound);
 
 module.exports = router;
--- api/routes/DorayakiRoutes.js
+++ api/routes/DorayakiRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const dorayakiController = require("../controllers/DorayakiController");
+const Authenticator = require("../middleware/Authenticator");
 
-router.get("/", dorayakiController.index);
-router.get("/:id", dorayakiController.detail);
-router.post("/create", dorayakiController.createDorayaki);
-router.post("/resep/create", dorayakiController.createRecipe);
-router.get("*", dorayakiController.notFound);
+router.get("/", Authenticator, dorayakiController.index);
+router.get("/:id", Authenticator, dorayakiController.detail);
+router.post("/create", Authenticator, dorayakiController.createDorayaki);
+router.post("/resep/create", Authenticator, dorayakiController.createRecipe);
+router.get("*", Authenticator, dorayakiController.notFound);
 
 module.exports = router;
